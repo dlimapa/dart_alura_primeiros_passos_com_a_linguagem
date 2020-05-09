d0993f732be466820fda2454c1b67c7a5ada8da1
Daniel de Lima <dlima@tre-pa.jus.br>
2020-05-09 20:09:55 -0300

    código com int e double

---
+++ aula_03/variaveis_inteiras.dart
@@ -0,0 +1,8 @@
+void main() {
+  int idade = 45;
+  int idade_irmao = idade + 2;
+  int idade_avo = (idade + idade_irmao) * 3;
+  print("Eu tenho $idade anos");
+  print("Idade do meu irmão é $idade_irmao");
+  print("Idade do meu avô é $idade_avo");
+}
